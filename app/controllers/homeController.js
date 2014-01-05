64ae2a6cd096949c81912a136f0a0ed8fa3e3d5f
Guilherme Henrique <guih@hotmail.com.br>
2014-01-05 00:10:59 -0200

    Intial commit

---
+++ app/controllers/homeController.js
@@ -0,0 +1,41 @@
+Main.controller('homeController', function($scope, $http) {
+   $scope.API = '41becea728ba6323a7402d268b6a5410'; // Your API Key
+
+    $scope.log = function(variable) {
+        console.log(variable);
+    }
+    $scope.alert = function(text) {
+        alert(text);
+    }
+
+    $scope.add = function(){
+      $scope.fetch( $scope.ui, '41becea728ba6323a7402d268b6a5410' );
+    }
+
+    $scope.fetch = function( q, apiKey ) {
+
+      $scope.apiKey = apiKey;
+      $scope.query = q;
+      $scope.url = 'http://api.soundcloud.com/tracks.json?';
+
+      $http.get(
+        $scope.url, { 
+
+          "q" : $scope.query,
+          "client_id" : $scope.apiKey,
+
+        }).success(function(data, status) {
+
+          console.log(data);
+          console.log('OKTRUE');
+
+      }).error(function(data, status) {
+
+          $scope.data = data || "Request failed";
+          $scope.status = status;        
+          console.log('OKFALSE', $scope.status, data);
+
+      });
+    };
+
+});
